b0a85b3d83cf9e1870730f0e527cb4630d8990e3
Lora Brown <drummergirllb@hotmail.com>
2021-06-22 22:10:48 -0700

    forgot to upload during my coding darnit

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -2,3 +2,32 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+//reference table body in html file
+var tbody = d3.select("tbody");
+
+//Console.log the data from data.js
+console.log(data);
+
+//Loop Through `data` and console.log each object
+data.forEach(function(dataTable) {
+    console.log(dataTable);
+
+//Use d3 to append one table row `tr` for each     
+var row = tbody.append("tr");
+
+//Use `Object.entries` to console.log each
+    Object.entries(dataTable).forEach(function([key, value]) {
+        console.log(key, value);
+
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
+
+
+// Input fields can trigger a change event when new text is entered. NOTE: I copied this cause I know Ill need it.
+//inputField.on("change", function() {
+//    var newText = d3.event.target.value;
+//    console.log(newText);
+//  });  
