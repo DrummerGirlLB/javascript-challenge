b7cd99b936ba30aaa047f616845462c52a332263
Lora Brown <drummergirllb@hotmail.com>
2021-06-26 15:33:25 -0700

    keep forgettting to push

--- Instructions/StarterCode/static/js/app2.js
+++ Instructions/StarterCode/static/js/app2.js
@@ -1,25 +1,13 @@
-// from data.js (to descriptive variable for later)
+
 var dataTable = data;
 
-// YOUR CODE HERE!
-
-//reference table body in html file
 var tbody = d3.select("tbody");
 
-//Console.log the data from data.js
-console.log(tbody);
-
-//Loop Through `data` and console.log each object
 data.forEach(function(dataTable) {
-    console.log(dataTable);
-
-//Use d3 to append one table row `tr` for each     
+   
     var row = tbody.append("tr");
 
-//Use `Object.entries` to console.log each
     Object.entries(dataTable).forEach(function([key, value]) {
-        console.log(key, value);
-
         var cell = row.append("td");
         cell.text(value);
     });
